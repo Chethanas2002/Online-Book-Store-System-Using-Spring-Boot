b4efa0c7d4954e45ef36314573ed4a00b07d219a
Chethana <chethanas1302@gmail.com>
2025-08-09 23:19:37 +0530

    -added one to one relationship between user and user details
    -made user as the owning side

--- OnlineBookStoreSystem/src/main/java/com/spring/OnlineBookStoreSystem/Model/User.java
+++ OnlineBookStoreSystem/src/main/java/com/spring/OnlineBookStoreSystem/Model/User.java
@@ -1,10 +1,15 @@
 package com.spring.OnlineBookStoreSystem.Model;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,4 +27,9 @@ public class User {
 	private String userName;
 	private String password;
 	private String role;
+	
+	@JsonManagedReference
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name="userDetailsId")
+	private UserDetails userDetails;
 }
--- OnlineBookStoreSystem/src/main/java/com/spring/OnlineBookStoreSystem/Model/UserDetails.java
+++ OnlineBookStoreSystem/src/main/java/com/spring/OnlineBookStoreSystem/Model/UserDetails.java
@@ -1,10 +1,14 @@
 package com.spring.OnlineBookStoreSystem.Model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -24,4 +28,8 @@ public class UserDetails {
 	private String email;
 	private String address;
 	private long phoneNumber;
+	
+	@JsonBackReference
+	@OneToOne(mappedBy = "userDetails",cascade = CascadeType.ALL)
+	private User user;
 }
