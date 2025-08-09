7fe79c98867a4b2915c923691b00a6c855dee13a
Chethana <chethanas1302@gmail.com>
2025-08-09 19:33:00 +0530

    - created initial project with the following dependicies 1. Spring Web 2. Spring Data JPA 3. Lombok 4. MySQL Driver 5. Spring Boot Dev tools

---
+++ OnlineBookStoreSystem/src/test/java/com/spring/OnlineBookStoreSystem/OnlineBookStoreSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.OnlineBookStoreSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookStoreSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
