0e66f77a36dea1fc597e7c402df06d6d2a7a1b36
Chethana <chethanas1302@gmail.com>
2025-08-11 14:07:33 +0530

    - added one to many relation between order and order items

--- OnlineBookStoreSystem/src/main/java/com/spring/OnlineBookStoreSystem/Model/OrderItem.java
+++ OnlineBookStoreSystem/src/main/java/com/spring/OnlineBookStoreSystem/Model/OrderItem.java
@@ -1,9 +1,13 @@
 package com.spring.OnlineBookStoreSystem.Model;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,4 +22,9 @@ public class OrderItem {
 	private int orderItemsId;
 	private int quantity;
 	private float priceAtPurchase;
+	
+	@JsonManagedReference
+	@ManyToOne
+	@JoinColumn(name="orderId")
+	private Orders order;
 }
--- OnlineBookStoreSystem/src/main/java/com/spring/OnlineBookStoreSystem/Model/Orders.java
+++ OnlineBookStoreSystem/src/main/java/com/spring/OnlineBookStoreSystem/Model/Orders.java
@@ -1,15 +1,19 @@
 package com.spring.OnlineBookStoreSystem.Model;
 
 import java.time.LocalDate;
+import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -31,4 +35,8 @@ public class Orders {
 	@ManyToOne
 	@JoinColumn(name = "userId")
 	private User user;
+	
+	@JsonBackReference
+	@OneToMany(mappedBy = "order" , cascade = CascadeType.ALL , orphanRemoval = true)
+	private List<OrderItem> orderItem;
 }
