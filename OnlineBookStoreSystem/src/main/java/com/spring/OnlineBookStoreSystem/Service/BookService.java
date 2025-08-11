63a362fd9ce2e3cfbde7e8309b38bebbb25bda10
Chethana <chethanas1302@gmail.com>
2025-08-11 17:57:16 +0530

    - added a new method that links book with author using both of these id

--- OnlineBookStoreSystem/src/main/java/com/spring/OnlineBookStoreSystem/Service/BookService.java
+++ OnlineBookStoreSystem/src/main/java/com/spring/OnlineBookStoreSystem/Service/BookService.java
@@ -6,7 +6,9 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.spring.OnlineBookStoreSystem.Model.Author;
 import com.spring.OnlineBookStoreSystem.Model.Book;
+import com.spring.OnlineBookStoreSystem.Repository.AuthorRepository;
 import com.spring.OnlineBookStoreSystem.Repository.BookRepository;
 
 @Service
@@ -43,4 +45,28 @@ public class BookService {
 		bookRepo.deleteById(id);
 		return "Book deleted successfully";
 	}
+	
+	
+	
+	@Autowired
+    private AuthorRepository authorRepository;
+
+    public void linkAuthorToBook(int bookId, int authorId) {
+        Book book = bookRepo.findById(bookId)
+                .orElseThrow(() -> new RuntimeException("Book not found"));
+
+        Author author = authorRepository.findById(authorId)
+                .orElseThrow(() -> new RuntimeException("Author not found"));
+
+        // Set the relation
+        book.setAuthor(author);
+
+        // Optional: keep bidirectional sync
+//        if (!author.getBook().contains(book)) {
+//            author.getBook().add(book);
+//        }
+
+        bookRepo.save(book);
+        authorRepository.save(author); // not always required if cascade is set
+    }
 }
